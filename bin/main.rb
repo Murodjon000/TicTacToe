490b32ed5bba5e590e88153361b18c1ae415cbe7
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-04 14:09:11 +0500

    added main.rb with its user-interface

---
+++ bin/main.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+playerx = Player.new
+playerx.sign = 'x'
+puts "Enter user 'X' name."
+playerx.name = gets.chomp
+while playerx.name =~ /[^a-zA-Z]/ || playerx.name == ''
+  puts 'Invalid name, enter another name'
+  playerx.name = gets.chomp
+end
+
+playero = Player.new
+  playero.sign = 'o'
+  puts "Enter user 'O' name."
+  playero.name = gets.chomp
+  while playero.name =~ /[^a-zA-Z]/ || playero.name == '' || playero.name == playerx.name
+    puts 'Invalid name, enter another name'
+    playero.name = gets.chomp
+  end
+
+
+game=TicTacToe.new
+
+
+  puts 'To see the game rules press any button!'
+  gets.chomp
+  puts "Each player shall select the number from table below! (only numbers from 1 to 9!)\n"
+  puts game.reference_board
+
+game.player1=playerx.name
+game.player2=playero.name
+game.play
