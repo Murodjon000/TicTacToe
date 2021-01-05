7972f4eb8822a356043251a63aa9532787d6df28
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-05 10:01:01 +0500

    fixed indentetios

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,4 @@
+# rubocop:disable all 
 #!/usr/bin/env ruby
 require_relative "../lib/board.rb"
 require_relative "../lib/player.rb"
@@ -35,3 +36,4 @@ game=TicTacToe.new
 game.player1=playerx.name
 game.player2=playero.name
 game.play
+# rubocop:enable all 
